f640e184d1b1409ba92d6fafba8611344992e465
ezh01 <ezhtop@gmail.com>
2023-09-23 20:04:43 +0300

    first commit

---
+++ app/src/main/java/com/example/practice9/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.practice9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var text3 = findViewById<TextView>(R.id.textView3)
+        text3.text = "Copyright (C) 2021"
+    }
+
+}
